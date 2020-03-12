08ec1aed6b8e82464c50cb10f26f051bc4b1cb3c
michalosinski91 <mosinski@hotmail.co.uk>
2020-03-12 11:51:56 +0100

    fetch script added

---
+++ infinite-scroll/script.js
@@ -0,0 +1,67 @@
+const postsContainer = document.getElementById("posts-container");
+const loaderEl = document.querySelector(".loader");
+const filterEl = document.getElementById("filter");
+
+let limit = 5;
+let page = 1;
+
+// Fetch posts from API
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+      }
+    }
+  );
+  const data = await res.json();
+  return data;
+}
+
+// show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach(post => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("posts__entry");
+    postEl.innerHTML = `
+    <div class="posts__number">${post.id}</div>
+    <div class="posts__info">
+      <h2 class="posts__title">${post.title}</h2>
+      <p class="posts__body">
+        ${post.body}
+      </p>
+    </div>
+    `;
+    postsContainer.appendChild(postEl);
+  });
+}
+
+// show loader and fetch more posts
+function showLoading() {
+  loaderEl.classList.add("loader--show");
+  setTimeout(() => {
+    loaderEl.classList.remove("loader--show");
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+// initial fetch
+showPosts();
+
+//Event listeners
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    // show the loader
+    showLoading();
+  }
+});
